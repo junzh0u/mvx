ca5821b97e4e2bb069eb3fb2851a5ad703de3b95
Jun Zhou <junz@junz.info>
2025-05-29 09:13:10 -0700

    logs

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
 use anyhow::bail;
 use anyhow::ensure;
 use clap::Parser;
+use clap_verbosity_flag::{InfoLevel, Verbosity};
 use colored::Colorize;
 use core::panic;
 use fs_extra::file::{CopyOptions, TransitProcess, move_file_with_progress};
 use indicatif::{HumanDuration, MultiProgress, ProgressBar, ProgressDrawTarget, ProgressStyle};
 use std::fs;
+use std::io::Write;
 use std::os::unix::fs::MetadataExt;
 use std::path::{Path, PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    /// Suppress progress bars and messages
-    #[arg(short, long)]
-    quiet: bool,
+    #[command(flatten)]
+    verbosity: Verbosity<InfoLevel>,
 
     /// Path to move from
     src: PathBuf,
@@ -27,11 +28,43 @@ pub struct Cli {
 ///
 /// Will return `Err` if move/merge fails for any reason.
 pub fn run(cli: &Cli) -> anyhow::Result<()> {
-    let start = std::time::Instant::now();
     let mp = MultiProgress::new();
-    if cli.quiet {
+    if cli.verbosity.log_level() < Some(log::Level::Info) {
         mp.set_draw_target(ProgressDrawTarget::hidden());
     }
+    let mp_clone = mp.clone();
+
+    let _ = env_logger::Builder::new()
+        .filter_level(cli.verbosity.log_level_filter())
+        .format(move |buf, record| {
+            let ts = chrono::Local::now().to_rfc3339().bold();
+            let file_and_line = format!(
+                "[{:>10}:{:<3}]",
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+            )
+            .italic();
+            let level = match record.level() {
+                log::Level::Error => "ERROR".red(),
+                log::Level::Warn => "WARN ".yellow(),
+                log::Level::Info => "INFO ".green(),
+                log::Level::Debug => "DEBUG".blue(),
+                log::Level::Trace => "TRACE".magenta(),
+            }
+            .bold();
+
+            let msg = format!("{ts} {file_and_line} {level} {}", record.args());
+
+            if mp_clone.is_hidden() {
+                writeln!(buf, "{msg}")
+            } else {
+                mp_clone.println(msg)
+            }
+        })
+        .try_init();
+
+    log::trace!("run({cli:?})");
+    let start = std::time::Instant::now();
     let pb_info = mp.add(
         ProgressBar::new_spinner()
             .with_style(ProgressStyle::default_spinner().tick_chars("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏")),
@@ -83,7 +116,6 @@ pub fn run(cli: &Cli) -> anyhow::Result<()> {
         ));
         merge_directories(src, &dest, &mp)?;
         pb_info.set_style(ProgressStyle::with_template("{msg}")?);
-        pb_info.set_prefix("✔");
         pb_info.finish_with_message(format!(
             "{} Merged in {}: '{}' => '{}'",
             "✔".green().bold(),
@@ -101,6 +133,11 @@ pub fn run(cli: &Cli) -> anyhow::Result<()> {
 }
 
 fn merge_directories(src: &PathBuf, dest: &Path, mp: &MultiProgress) -> anyhow::Result<()> {
+    log::trace!(
+        "merge_directories('{}', '{}')",
+        src.display(),
+        dest.display()
+    );
     let files = collect_files(src)?;
     let pb_files = mp.add(
         ProgressBar::new(files.len() as u64).with_style(
@@ -127,13 +164,17 @@ fn merge_directories(src: &PathBuf, dest: &Path, mp: &MultiProgress) -> anyhow::
 }
 
 fn recur_remove_dir(dir: &PathBuf) -> std::io::Result<()> {
+    log::trace!("recur_remove_dir('{}')", dir.display());
     for entry in fs::read_dir(dir)? {
         recur_remove_dir(&entry?.path())?;
     }
-    fs::remove_dir(dir)
+    fs::remove_dir(dir)?;
+    log::debug!("Removed empty directory: '{}'", dir.display());
+    Ok(())
 }
 
 fn move_file(src: &PathBuf, dest: &PathBuf, mp: &MultiProgress) -> anyhow::Result<()> {
+    log::trace!("move_file('{}', '{}')", src.display(), dest.display());
     let src_meta = fs::metadata(src)?;
 
     let dest_abs = std::path::absolute(dest)?;
@@ -141,6 +182,7 @@ fn move_file(src: &PathBuf, dest: &PathBuf, mp: &MultiProgress) -> anyhow::Resul
         fs::create_dir_all(dest_parent)?;
         if src_meta.dev() == fs::metadata(dest_parent)?.dev() {
             fs::rename(src, dest)?;
+            log::debug!("Renamed: '{}' => '{}'", src.display(), dest.display());
             return Ok(());
         }
     }
@@ -158,6 +200,7 @@ fn move_file(src: &PathBuf, dest: &PathBuf, mp: &MultiProgress) -> anyhow::Resul
     };
     let options = CopyOptions::new().overwrite(true);
     move_file_with_progress(src, dest, &options, progress_handler)?;
+    log::debug!("Moved: '{}' => '{}'", src.display(), dest.display());
     pb_bytes.finish_and_clear();
     mp.remove(&pb_bytes);
     Ok(())
